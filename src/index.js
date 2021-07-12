dadbdf8dd91031f3edbf5831c3c68c70f455eaa4
Harshith Mullapudi <harshithmullapudi@gmail.com>
2021-07-12 19:59:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-serverless')
 const { Cluster } = require('puppeteer-cluster')
 
 const { makeScreenshot } = require('./screenshot.js')
